9429b3204d94a925f8ecfa3d37f3b0f6e2e7655f
zhoushineyoung <zhoushineyoung@hotamil.com>
2014-07-25 23:18:49 +0800

    Converting an object[request.headers] to JSON_string

--- libs/Server.js
+++ libs/Server.js
@@ -17,16 +17,13 @@ function service(route, handle, cluster) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
 
-        var heasers = {};
-        heasers = request.headers;
-        console.info('heasers = ' + request.headers);
+        var headers = request.headers;
+        console.info('headers = ' + JSON.stringify(headers));
 
         dataLogger.info("Http" + request.httpVersion + ", [" + request.method + "]," + " Request for [" + pathname + "] received.");
-        dataLogger.error('dataLogger-error-test.');
         route(handle, pathname, response);
     }
     http.createServer(onRequest).listen(config['http']['port'], config['http']['ip']);
-    logger.error('logger-error-test.');
     logger.info('Server[%d], with pid[%d], has started.',cluster.worker.id, process.pid);
 }
 exports.service = service;
